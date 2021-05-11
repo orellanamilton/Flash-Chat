74567a605f1ae8fc0bc19b2ea2831e7a40a5e8e8
Milton Orellana <Orellanamilton@hotmail.com>
2021-05-10 22:39:01 -0300

    added new functionality

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Milton Orellana on 09/05/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Milton Orellana on 07/05/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
